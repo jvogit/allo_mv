e7b572775e5da7db22851d2a2001a8323850a94e
jvogit <justinxvopro@gmail.com>
2023-11-15 17:50:20 -0800

    jv - port over mblewis to new allolib

---
+++ mblewis/final_project/demos/sound-environment.cpp
@@ -0,0 +1,252 @@
+// sound-lines - a simple spectrogram-like output of a sampled audio clip
+// Mitchell Lewis, 2-20-19
+
+
+#include "al/core.hpp"
+using namespace al;
+
+#include "Gamma/DFT.h"
+#include "Gamma/SamplePlayer.h"
+using namespace gam;
+
+#include <vector>
+using namespace std;
+
+#define N (513)  // number of particles
+#define DISP_WIDTH (5.0)
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------------------
+//                                          APP START
+// --------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------------------
+
+
+struct AlloApp : App {
+    ShaderProgram shader;
+    Texture texture;
+    
+    Mesh pointMesh;          // holds positions
+
+    gam::SamplePlayer<float, gam::ipl::Linear, gam::phsInc::Loop> samplePlayer;
+    gam::STFT stft;
+    AudioIO audio;
+
+    vector<float> spectrum;
+    
+    // measures magnitude of each band and records it as a distance\
+    // also keeps track of max value per band for normalization
+    vector<float> bandDist;
+    vector<float> max;
+    
+    vector<vector<float>> waves;
+    float waveWidth = 1000;
+    float waveHeight = 5;
+    float xWidth = 10;
+    float zWidth = 10;
+    float waveRadius = 5;
+    
+    
+    vector<int> bandWidths{ 2,2,2,2,2,3,3,3,3,4,4,5,6,7,8,10,12,15,19,23,28,38,53,75,90,94 }; // Bark Scale - # of stft bands per Bark band
+    float heightScale = 10;
+    bool analyzeInput = true; // if true: analyze input channel, else: analyze sample
+    bool displayMode = false;  // true: show equal-width rectangle bands, false: show all bands
+    
+    void onCreate() override {
+        if(!analyzeInput){
+            samplePlayer.load("../sound/10.wav");
+            samplePlayer.loop();
+        }
+        Sync::master().spu(audioIO().fps());
+        audioIO().print();
+        
+        int n = bandWidths.size();
+
+        
+        for(int i=0; i<n; i++){
+            bandDist.push_back(0);
+            max.push_back(0.1);
+            
+            vector<float> wave;
+            for(int j=0; j<waveWidth; j++){
+                wave.push_back(0);
+            }
+            waves.push_back(wave);
+        }
+        
+        //Vec3d(0.000000, 17.495046, 46.419401);
+        //Quatd(0.987681, -0.156482, 0.000000, 0.000000);
+        nav().pos(0.000000, 17.495046, 46.419401);
+        nav().quat(Quatd(0.987681, -0.156482, 0.000000, 0.000000));
+
+    }
+
+    void drawWaves(Graphics& g, vector<vector<float>> waves){
+        for(int i=0; i<waves.size(); i++){
+            Mesh m{Mesh::LINE_STRIP};
+            float wavePercent = 1 - (float) (i+1) / waves.size();  //
+            for(int j=0; j<waves[i].size(); j++){
+                float valPercent = (float) j / waves[i].size();
+                float val = waves[i][j];
+                
+                float x = (waveRadius + wavePercent*xWidth)*cos(valPercent*2*M_PI);
+                float z = (waveRadius + wavePercent*xWidth)*sin(valPercent*2*M_PI);
+                
+                float y = val*waveHeight + (wavePercent*5);
+                
+                m.vertices().push_back( Vec3f(x, val*waveHeight, z) );
+                m.color( HSV(wavePercent, val, val) );
+                
+            }
+            g.meshColor();
+            g.draw(m);
+        }
+    }
+    
+    
+    float radius = 4.0;
+    void pillar(Graphics& g, float height, float angle){
+        if(height == 0) return;
+        
+        float color = abs(angle/M_PI);
+        if(angle < 0) color = 1 - color;
+        
+        Vec3f bandCenter(radius*cos(angle), 0, radius*sin(angle));
+        Vec3f bandCross = bandCenter.cross(Vec3f(0, 1, 0));
+        
+        Vec3f vertexA = (bandCenter + bandCenter/10) - (bandCross/20);
+        Vec3f vertexB = (bandCenter + bandCenter/10) + (bandCross/20);
+        Vec3f vertexC = (bandCenter - bandCenter/10) - (bandCross/20);
+        Vec3f vertexD = (bandCenter - bandCenter/10) + (bandCross/20);
+        
+        Vec3f vertexE = vertexA + Vec3f(0, height*radius, 0);
+        Vec3f vertexF = vertexB + Vec3f(0, height*radius, 0);
+        Vec3f vertexG = vertexC + Vec3f(0, height*radius, 0);
+        Vec3f vertexH = vertexD + Vec3f(0, height*radius, 0);
+        //cout << vertexA  << vertexD << endl;
+        
+        Mesh m{Mesh::TRIANGLE_STRIP};
+        m.vertex(vertexA);
+        m.vertex(vertexE);
+        m.vertex(vertexF);
+        m.vertex(vertexB);
+        m.vertex(vertexA);
+        
+        m.vertex(vertexE);
+        m.vertex(vertexG);
+        m.vertex(vertexC);
+        
+        m.vertex(vertexA);
+        m.vertex(vertexB);
+        m.vertex(vertexD);
+        
+        m.vertex(vertexH);
+        m.vertex(vertexF);
+        m.vertex(vertexB);
+        
+        m.vertex(vertexD);
+        m.vertex(vertexH);
+        m.vertex(vertexG);
+        
+        m.vertex(vertexC);
+        m.vertex(vertexD);
+        m.vertex(vertexA);
+        
+        
+        
+        g.color( HSV(color, abs(height), abs(height)) );
+        g.draw(m);
+    }
+
+    
+    void onSound(AudioIOData& io) override {
+        vector<Vec3f>& vertex(pointMesh.vertices());
+        while (io()) {
+            float s;
+            if(analyzeInput) s = io.in(1);
+            else {
+                s = samplePlayer();
+                float f = samplePlayer.read(0);
+                io.out(0) = f;
+                io.out(1) = f;
+            }
+            if (stft(s)){
+                int currentBand = 0;
+                
+                // for each band in Bark scale
+                for(int i=0; i<bandWidths.size(); i++){
+                    float barkSum = 0;
+                    
+                    // for each STFT band
+                    for(int j=0; j<bandWidths[i]; j++){
+                        barkSum += stft.bin(currentBand).norm() * 1000;
+                        currentBand++;
+                    }
+                    
+                    // calculate mean height of Bark band
+                    float dist = barkSum / bandWidths[i];
+                    
+                    // update max if necessary
+                    if(dist > max[i]) max[i] = dist;
+                    float distRatio = dist/max[i];
+                    
+                    // set point distance to normalized band amplitude
+                    float changeVal = (distRatio - bandDist[i]);
+                    bandDist[i] += changeVal/20;
+                    
+                }
+            }
+        }
+    }
+    
+    void onAnimate(double dt) override {
+        for(int i=0; i<waves.size(); i++){
+            waves[i].pop_back();
+            waves[i].insert(waves[i].begin(), bandDist[i]);
+        }
+    }
+    
+    void onDraw(Graphics& g) override {
+        g.clear(0);
+        
+        g.depthTesting(true);
+        g.blending(true);
+        g.blendModeTrans();
+        
+        drawWaves(g, waves);
+        
+        for(int i=0; i<bandWidths.size(); i++){
+            float angle = i * (M_PI / bandWidths.size());
+            pillar(g,bandDist[i], angle);
+            pillar(g,bandDist[bandDist.size()-1-i], -angle);
+        }
+        
+    }
+    
+    void onKeyDown(const Keyboard& k) override {
+        switch (k.key()) {
+            case 'o':
+                displayMode = !displayMode;
+                
+            
+        }
+        //
+    }
+    
+    bool getAnalysisMode() { return analyzeInput; }
+};
+
+
+
+int main() {
+    AlloApp app;
+    if(app.getAnalysisMode()){
+        app.initAudio(44100, 512, -1,-1, 3);
+    }
+    else{
+        app.initAudio();
+    }
+    app.start();
+    
+}
---
+++ mblewis/final_project/seeing-sound.cpp
@@ -0,0 +1,521 @@
+/*
+ Seeing Sound
+ 
+ Final Project for MAT201B
+ by Mitchell Lewis
+ 
+ Adapted from DistributedApp by Andres Cabrera
+ 
+ Components:
+    Pillars -  demonstrates 3D mesh drawing, visualizes current sound output
+    Waves   -  demonstrate polar calculation, visualizes historical sound output
+    Clouds  -  demonstrate simulation, visualizes partial historical sound output
+ 
+ */
+
+
+
+#include <stdio.h>
+#include "al/app/al_App.hpp"
+#include "al/math/al_StdRandom.hpp"
+#include "al/app/al_DistributedApp.hpp"
+using namespace al;
+
+#include "Gamma/DFT.h"
+#include "Gamma/SamplePlayer.h"
+using namespace gam;
+
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Number of bands in state() storage
+#define BAND_N (26)
+
+// Number of historical sound values to store
+#define HIST_N (1000)
+
+// -----------------------------------------------------------------------
+//  Variables
+// -----------------------------------------------------------------------
+
+// Plays and sends sound file data
+gam::SamplePlayer<float, gam::ipl::Linear, gam::phsInc::Loop> samplePlayer;
+
+// Analyzes sound file data
+gam::STFT stft;
+
+// Hard-coded band widths for each band of Bark scale (with a split in the last band)
+// Specifies number of FFT bands to include in each Bark sum
+std::vector<int> bandWidths{
+    2, 2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  5, 6, 7,
+    8, 10, 12, 15, 19, 23, 28, 38, 53, 75, 90, 94};
+
+// Stores max value of each band for normalization
+std::vector<float> bandMax;
+
+// Stores location and color of each pillar
+std::vector<Mesh> pillars;
+
+// Inner radius of pillars, used to determine length of pillars as well
+float pillarRadius = 2.0;
+
+// Number of values to display across wave circle
+float waveWidth = HIST_N;
+
+// Maximum height of each wave
+// Each wave is also incremented by this height as they move outwards
+float waveHeight = 4.0;
+
+// Inner radius of waves
+float waveRadius = 3.0;
+
+// Maximum radius for clouds to move within
+float maxRadius = 10.0;
+
+// Total number of clouds to render
+int numClouds = BAND_N*5;
+
+// Number of vertices within each cloud
+int cloudSize = HIST_N/4;
+
+// Stores cloud meshes
+vector<Mesh> clouds;
+
+// Stores cloud positions
+vector<Vec3f> cloudPos;
+
+// Stores cloud velocities
+vector<Vec2f> cloudVelocity;
+
+// Background value / lightness
+float background = 0.0;
+
+// Control: if true, nothing is drawn/animated/analyzed; controlled by space bar
+bool paused = true;
+
+
+// -----------------------------------------------------------------------
+//  Shared State
+// -----------------------------------------------------------------------
+
+struct SharedState {
+    
+    // Stores/shares the last HIST_N sound values for each Bark band
+    float soundVals[BAND_N][HIST_N];
+    
+    // Stores/shares current index to write
+    int soundIndex;
+    
+    // Stores/shares current pose of nav()
+    Pose pose;
+};
+
+// -----------------------------------------------------------------------
+//  App Start
+// -----------------------------------------------------------------------
+
+class DistributedExampleApp : public DistributedAppWithState<SharedState> {
+public:
+    AudioDomain audioDomain;
+
+    DistributedExampleApp(){
+        if(hasCapability(CAP_RENDERING)){
+            displayMode(displayMode() | Window::STEREO_BUF);
+        }
+        configureAudio(44100, 1024, 60, 60);
+    }
+    
+// -----------------------------------------------------------------------
+//  Create
+// -----------------------------------------------------------------------
+    
+    virtual void onCreate() override {
+
+        // Loads song, sets it to loop on completion
+        samplePlayer.load("../sound/10.wav");
+        samplePlayer.loop();
+        
+        Sync::master().spu(audioIO().fps());
+        
+        createPillars();
+        createClouds();
+        
+        // Init analysis arrays / index
+        state().soundIndex = 0;
+        for(int i=0; i<BAND_N; i++){
+            bandMax.push_back(0.1);
+            for(int j=0; j<HIST_N; j++){
+                state().soundVals[i][j] = 0;
+            }
+        }
+        
+        // Set initial pose
+        nav().pos(0.000000, 1.5, 0);
+        
+    }
+    
+    void createPillars() {
+        // Each band has 2 pillars, directly across from each other
+        // This ensures that the full spectrum of sound is visible from any vantage point
+        for (int i = 0; i < 2 * BAND_N; i++) {
+            float angle = i * (M_PI / BAND_N);
+            
+            // Calculates center of each pillar and a vector orthogonal to origin->center for finding sides
+            Vec3f bandCenter(pillarRadius * cos(angle), 0, pillarRadius * sin(angle));
+            Vec3f bandCross = bandCenter.cross(Vec3f(0, 1, 0));
+            
+            // Four bottom corners
+            Vec3f vertexA = (bandCenter + bandCenter / 10) - (bandCross / 20);
+            Vec3f vertexB = (bandCenter + bandCenter / 10) + (bandCross / 20);
+            Vec3f vertexC = (bandCenter - bandCenter / 10) - (bandCross / 20);
+            Vec3f vertexD = (bandCenter - bandCenter / 10) + (bandCross / 20);
+            
+            // Four top corners
+            Vec3f vertexE = vertexA + Vec3f(0, pillarRadius, 0);
+            Vec3f vertexF = vertexB + Vec3f(0, pillarRadius, 0);
+            Vec3f vertexG = vertexC + Vec3f(0, pillarRadius, 0);
+            Vec3f vertexH = vertexD + Vec3f(0, pillarRadius, 0);
+            
+            // Creates Mesh that draws a solid rectangular prism
+            Mesh m{Mesh::TRIANGLE_STRIP};
+            m.vertex(vertexA);  // 0
+            m.vertex(vertexE);  // 1 -- Each highlighted value is changed over time to control height
+            m.vertex(vertexF);  // 2 --
+            m.vertex(vertexB);  // 3
+            m.vertex(vertexA);  // 4
+            
+            m.vertex(vertexE);  // 5 --
+            m.vertex(vertexG);  // 6 --
+            m.vertex(vertexC);  // 7
+            
+            m.vertex(vertexA);  // 8
+            m.vertex(vertexB);  // 9
+            m.vertex(vertexD);  // 10
+            
+            m.vertex(vertexH);  // 11 --
+            m.vertex(vertexF);  // 12 --
+            m.vertex(vertexB);  // 13
+            
+            m.vertex(vertexD);  // 14
+            m.vertex(vertexH);  // 15 --
+            m.vertex(vertexG);  // 16 --
+            
+            m.vertex(vertexC);  // 17
+            m.vertex(vertexD);  // 18
+            m.vertex(vertexA);  // 19
+            
+            pillars.push_back(m);
+        }
+    }
+    
+    void createClouds(){
+        // Initializes cloud mesh, random position (with controlled height), color according to band, and random velocity
+        for(int i=0; i<numClouds; i++){
+            
+            Mesh cloud{Mesh::TRIANGLE_FAN};
+            Vec3f center(rnd::normal() * (maxRadius/3), (float(i) * 0.0005) + 2*(1+waveHeight), rnd::normal() * (maxRadius/3));
+            
+            // for each point in cloud
+            for(int j=0; j<cloudSize; j++){
+                cloud.color(HSV(float(i%BAND_N)/BAND_N, 0.25, 1));
+            }
+            
+            clouds.push_back(cloud);
+            cloudPos.push_back(center);
+            cloudVelocity.push_back(Vec2f(rnd::normal()*2, rnd::normal()*2));
+        }
+    }
+    
+    // -----------------------------------------------------------------------
+    //  Draw
+    // -----------------------------------------------------------------------
+    
+    virtual void onDraw(Graphics& g) override {
+        if(paused) return;
+        
+        if (hasCapability(CAP_RENDERING) || hasCapability(CAP_CONSOLE_IO) ||
+            hasCapability(CAP_RENDERING)) {
+            
+            g.clear(0);
+            g.depthTesting(true);
+            g.blending(true);
+            g.blendTrans();
+            // g.blendModeTrans();
+            
+            drawWaves(g);
+            drawPillars(g);
+            drawClouds(g);
+        }
+        
+    }
+    
+    void drawPillars(Graphics& g) {
+        
+        // For each pillar
+        for (int i = 0; i < 2 * BAND_N; i++) {
+            float height, color;
+            
+            // Finds height / color for pillars in first half of circle
+            if (i < BAND_N) {
+                color = (float)i / BAND_N;
+                height = state().soundVals[i][state().soundIndex];
+            }
+            
+            // Finds height / color for pillars in second half of circle
+            else {
+                color = (float)(i - BAND_N) / BAND_N;
+                height = state().soundVals[i - BAND_N][state().soundIndex];
+            }
+            
+            // updates height
+            pillars[i].vertices()[1].y = pillarRadius * height;
+            pillars[i].vertices()[2].y = pillarRadius * height;
+            
+            pillars[i].vertices()[5].y = pillarRadius * height;
+            pillars[i].vertices()[6].y = pillarRadius * height;
+            
+            pillars[i].vertices()[11].y = pillarRadius * height;
+            pillars[i].vertices()[12].y = pillarRadius * height;
+            
+            pillars[i].vertices()[15].y = pillarRadius * height;
+            pillars[i].vertices()[16].y = pillarRadius * height;
+            
+            // Updates color:
+            //  hue range is from 0.33 - 0.667 (according to band)
+            //  saturation range is from 0.5 - 1.0 (according to height)
+            //  value range is from 0.667 - 1.0 (according to height)
+            g.color(HSV(0.33+color*0.667, (height+5)/10, (height+2)/3));
+            
+            g.draw(pillars[i]);
+        }
+    }
+    
+    void drawWaves(Graphics& g) {
+        
+        // For each band, draws one wave
+        for (int i = 0; i < BAND_N; i++) {
+            
+            Mesh m{Mesh::TRIANGLE_STRIP};
+            
+            // Inner wave is 100%, outer wave is 0%
+            float wavePercent = 1 - (float)(i + 1) / BAND_N;
+            
+            // For each stored sound value, one vertex is added
+            for (int j = 0; j < HIST_N; j++) {
+                
+                // Percentage through circle
+                float valPercent = (float)j / HIST_N;
+                
+                // Finds corresponding sound value
+                float val = state().soundVals[i][(j+state().soundIndex)%HIST_N];
+                
+                // Polar coordinates to determine place in circle
+                float x = (waveRadius + (wavePercent) * waveRadius) * cos(valPercent * 2 * M_PI);
+                float z = (waveRadius + (wavePercent) * waveRadius) * sin(valPercent * 2 * M_PI);
+                
+                // Height according to sound value
+                float y = (val * waveHeight) + (wavePercent * waveHeight);
+                
+                // Pushes a vertex at height and 0 to create solid wave
+                m.vertices().push_back(Vec3f(x, y, z));
+                m.vertices().push_back(Vec3f(x, 0, z));
+                
+                // Color:
+                //  hue is from 0 - 0.33
+                //  saturation is from 0.5 - 1.0
+                //  value is from 0.3 - 0.7
+                m.color(HSV(wavePercent*0.33, (val+1)/2, (val*0.7)+0.3));
+                
+                // Lower value is darker to create gradient that highlights the waves in front
+                m.color(HSV(wavePercent*0.33, (val+1)/2, (val*0.7)));
+            }
+            
+            g.meshColor();
+            g.draw(m);
+        }
+    }
+    
+    void drawClouds(Graphics& g){
+        for(int i=0; i<clouds.size(); i++){
+            g.meshColor();
+            g.draw(clouds[i]);
+        }
+    }
+    
+    // -----------------------------------------------------------------------
+    //  Animate
+    // -----------------------------------------------------------------------
+    
+    void onAnimate(double dt) override {
+        if(paused) return;
+        
+        if (hasCapability(CAP_RENDERING)) {
+            
+            // Sets pose to saved pose
+            // pose() = state().pose;
+            
+        }
+        
+        
+        animateClouds();
+        
+    }
+    
+    
+    virtual void animateClouds(){
+        // Separate cloud iterator since it loops according to band
+        int cloudIterator = 0;
+        
+        // for each band
+        for(int i=1; i<BAND_N; i++){
+            
+            // for each cloud in band[i]
+            for(int j=0; j<(numClouds/BAND_N); j++){
+                
+                // Finds distance from center to determine if it is within radius
+                float dist = sqrt((cloudPos[cloudIterator].x * cloudPos[cloudIterator].x)+(cloudPos[cloudIterator].z * cloudPos[cloudIterator].z));
+                
+                // Bounces if it reaches max radius
+                if(dist >= maxRadius) cloudVelocity[cloudIterator] *= -1;
+                
+                // Places clouds slightly above max wave height, and each "band" of clouds is slightly above the last
+                float height = (float(cloudIterator) * 0.005) + 2*(waveHeight);
+                
+                // hue ranges from 0.5 - 1.0 (according to band)
+                float hue = 0.5 + 0.5*float(i)/BAND_N;
+                
+                
+                // get last center point
+                Vec3f center = cloudPos[cloudIterator];
+                
+                // Moves center based on velocity * current sound value
+                // This causes them to jump and dance during loud sounds
+                center.x += cloudVelocity[cloudIterator].x * 0.01 * state().soundVals[i][state().soundIndex]*2;
+                center.z += cloudVelocity[cloudIterator].y * 0.01 * state().soundVals[i][state().soundIndex]*2;
+                
+                // Update height
+                center.y = height;
+                
+                // Update stored center position
+                cloudPos[cloudIterator] = center;
+                
+                // Clear positions and colors for update
+                clouds[cloudIterator].vertices().clear();
+                clouds[cloudIterator].colors().clear();
+                
+                // Init cloud at new center with darker color to create gradient moving outwards
+                clouds[cloudIterator].vertex(center);
+                clouds[cloudIterator].color(HSV(hue, 0.1, 0.5));
+                
+                Vec3f soundPoint;
+                float soundVal;
+                
+                //for each point in cloud[cloudIterator]
+                for(int k=0; k<cloudSize; k++){
+
+                    // find sound value and clamp to 0-1 (just in case)
+                    float rawVal =  state().soundVals[i][(state().soundIndex - (k+HIST_N))%HIST_N];
+                    soundVal = rawVal <= 0 ? 0 : rawVal >= 1 ? 1 : rawVal;
+                    
+                    float angle = (float(k)/cloudSize) * M_2PI;
+                    
+                    // find point based on sound val and position in circle
+                    soundPoint = Vec3f(center.x+(soundVal*cos(angle)), height, center.z+(soundVal*sin(angle)));
+                    
+                    // Add new vertex to mesh
+                    clouds[cloudIterator].vertex(soundPoint);
+                    
+                    // Add color:
+                    //  Hue according to band
+                    //  Saturation between 0.125 - 0.625
+                    //  Value between 0.667 - 1.0
+                    clouds[cloudIterator].color(HSV(hue, (0.25+soundVal)/2, (2+soundVal)/3));
+                    
+                }
+                
+                // Add point averaged between first and last edge points to smooth updating edge
+                Vec3f avePoint = (clouds[cloudIterator].vertices()[1] + clouds[cloudIterator].vertices()[cloudSize]) / 2;
+                clouds[cloudIterator].vertex(avePoint);
+                clouds[cloudIterator].color(clouds[cloudIterator].colors()[1]);
+                
+                cloudIterator++;
+                
+            }
+        }
+        
+        
+    }
+    
+    // -----------------------------------------------------------------------
+    //  Sound
+    // -----------------------------------------------------------------------
+    
+    virtual void onSound(AudioIOData& io) override {
+        // Audio will recieve state from simulator
+        while (io() && !paused) {
+            if (stft(samplePlayer())) {
+                int currentBand = 0;
+                
+                // Update soundVal index so that it iterates circularly
+                state().soundIndex = (1+state().soundIndex)%HIST_N;
+                
+                // for each band in Bark scale
+                for (int i = 0; i < BAND_N; i++) {
+                    float barkSum = 0;
+                    
+                    // sums each FFT band [j] within Bark band [i]
+                    for (int j = 0; j < bandWidths[i]; j++) {
+                        barkSum += stft.bin(currentBand).norm() * 1000;
+                        currentBand++;
+                    }
+                    
+                    // calculate mean amplitude of Bark band
+                    float dist = barkSum / bandWidths[i];
+                    
+                    // update max if necessary
+                    if (dist > bandMax[i]) bandMax[i] = dist;
+                    
+                    // create ratio of current amplitude to max amplitude
+                    float distRatio = dist / bandMax[i];
+                    
+                    // find difference between last value and current value
+                    // divide by 20 to smooth changes
+                    float changeVal = (distRatio - state().soundVals[i][(state().soundIndex-1)%HIST_N]) / 20;
+                    
+                    // set current value to last value + change
+                    state().soundVals[i][state().soundIndex] = state().soundVals[i][(state().soundIndex-1)%HIST_N] + changeVal;
+                }
+            }
+            
+            // output current sound frame to all channels such that they add up to original amplitude/volume
+            float f = samplePlayer.read(0) / io.channelsOut();
+            for(int i=0; i<io.channelsOut(); i++){
+                io.out(i) = f;
+            }
+        }
+    }
+    
+    // -----------------------------------------------------------------------
+    //  Control
+    // -----------------------------------------------------------------------
+    
+    bool onKeyDown(const Keyboard& k) override {
+        
+        // on spacebar press, pause output and reset sample player
+        if (k.key() == ' ') {
+            paused = !paused;
+            samplePlayer.reset();
+        }
+
+        return true;
+    }
+    
+};
+
+int main() {
+    DistributedExampleApp app;
+    app.start();
+    return 0;
+}
